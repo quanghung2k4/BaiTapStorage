11a4ff89424ce7cccc715d4e61e061a72ba73e8e
quanghung2k4 <quanghungbg2021@gmail.com>
2026-03-27 10:58:32 +0700

    edit main_activity task #09

--- app/src/main/java/com/learn/baitapstorage/MainActivity.java
+++ app/src/main/java/com/learn/baitapstorage/MainActivity.java
@@ -1,24 +1,60 @@
 package com.learn.baitapstorage;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.learn.baitapstorage.activities.LoginActivity;
+import com.learn.baitapstorage.activities.MoviesActivity;
+import com.learn.baitapstorage.activities.MyTicketsActivity;
+import com.learn.baitapstorage.activities.ShowtimesActivity;
+import com.learn.baitapstorage.activities.TheatersActivity;
+import com.learn.baitapstorage.dal.SharedPrefsManager;
 
 public class MainActivity extends AppCompatActivity {
+    private View btnMovies, btnTheaters, btnShowtimes, btnMyTickets, btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        
+        // Check login session
+        if (!SharedPrefsManager.isLoggedIn(this)) {
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+            return;
+        }
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnMovies = findViewById(R.id.btnMovies);
+        btnTheaters = findViewById(R.id.btnTheaters);
+        btnShowtimes = findViewById(R.id.btnShowtimes);
+        btnMyTickets = findViewById(R.id.btnMyTickets);
+        btnLogout = findViewById(R.id.btnLogout);
+
+        btnMovies.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, MoviesActivity.class));
+        });
+
+        btnTheaters.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, TheatersActivity.class));
+        });
+
+        btnShowtimes.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, ShowtimesActivity.class));
+        });
+
+        btnMyTickets.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, MyTicketsActivity.class));
+        });
+
+        btnLogout.setOnClickListener(v -> {
+            SharedPrefsManager.clearLogin(this);
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
         });
     }
 }
